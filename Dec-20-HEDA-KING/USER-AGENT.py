651ebfd155aa4628c256adc85923399133292c57
NILL-XD <139593713+Nill404-Cyber@users.noreply.github.com>
2023-12-20 15:56:52 +0600

    Update and rename FB-KING-USER-AGENT.txt to USER-AGENT.py

---
+++ Dec-20-HEDA-KING/USER-AGENT.py
@@ -0,0 +1,6 @@
+for xd in range(10000):
+    rr = random.randint; rc = random.choice
+    gt = ['N4LEFH','TQ2A','QQ1B','PQ1A','SQ3A','RD1B','LDK2WU','SD2A','AB03E','Z367Q','R8638','C886H'] 
+    strvgt = f"viabrowser;Safary-Mozilla/5.0 (Windows NT 10.0 .{str(rr(1,20))}; WOW64){str(rc(gt))})Applewebkit/537.36 (KHTML, like Gecko) Chrome/{str(rr(50,140))}.0.{str(rr(3990,5001))}.{str(rr(20,170))} Safari/537.36 Vivaldi/6.0.2979.18"
+    uateddy = random.choice([strvgt])
+    ugen.append(uateddy)
